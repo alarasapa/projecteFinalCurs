cd6fcb16fde99d4fce665d0154a7505a49ebc096
alarasapa <61461890+alarasapa@users.noreply.github.com>
2021-04-15 20:00:26 +0200

    Configuracion estilos y formulario, js configuracion usuario

--- ProyectoLaravel/PFCTennis/resources/views/ClientVista/home.blade.php
+++ ProyectoLaravel/PFCTennis/resources/views/ClientVista/home.blade.php
@@ -18,7 +18,120 @@
 @section('titol', 'Configuració')
 @section('content')
 
-<div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
-        
-</div>
+@push('css')
+    <link rel="stylesheet" href="{{ url('css/ClientEstils/configuracio.css') }}" >
+@endpush
+@push('scripts')
+    <script src="{{ url('js/ClientJS/configuracio.js') }}">
+        window.onload() = function(){init()};
+    </script>
+@endpush
+
+    <h1>BENVOLGUT A LA TEVA CONFIGURACIÓ: {{ Auth::user()->nom }}</h1>
+    
+    <form id="formConfiguració">
+        @csrf
+
+        <input id="rol" name="rol" type="hidden" value="U">
+
+        <div class="form-group row">
+            <label for="nom" class="col-md-4 col-form-label text-md-right">{{ __('Nom') }}</label>
+
+            <div class="col-md-6">
+                <input id="nom" type="text" class="form-control @error('name') is-invalid @enderror" name="nom" value="{{ Auth::user()->nom }}" required autocomplete="nom" autofocus>
+
+                @error('name')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
+            </div>
+        </div>
+
+        <div class="form-group row">
+            <label for="cognoms" class="col-md-4 col-form-label text-md-right">{{ __('Cognoms') }}</label>
+
+            <div class="col-md-6">
+                <input id="cognoms" type="text" class="form-control @error('name') is-invalid @enderror" name="cognoms" value="{{ Auth::user()->cognoms }}" required>
+
+                @error('name')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
+            </div>
+        </div>
+
+        <div class="form-group row">
+            <label for="dataNaixement" class="col-md-4 col-form-label text-md-right">{{ __('Data Naixement') }}</label>
+
+            <div class="col-md-6">
+                <input id="dataNaixement" type="date" class="form-control @error('name') is-invalid @enderror" name="dataNaixement" value="{{ Auth::user()->dataNaixement }}">
+
+                @error('name')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
+            </div>
+        </div>
+
+        <div class="form-group row">
+            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('Adreça Email') }}</label>
+
+            <div class="col-md-6">
+                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ Auth::user()->email }}" required autocomplete="email">
+
+                @error('email')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
+            </div>
+        </div>
+
+        <div class="form-group row">
+            <label for="telefon" class="col-md-4 col-form-label text-md-right">{{ __('Telefon') }}</label>
+
+            <div class="col-md-6">
+                <input id="telefon" type="text" class="form-control @error('name') is-invalid @enderror" name="telefon" value="{{ Auth::user()->telefon }}" required>
+
+                @error('name')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
+            </div>
+        </div>
+
+        <div class="form-group row">
+            <label for="contrasenya" class="col-md-4 col-form-label text-md-right">{{ __('Contrasenya') }}</label>
+
+            <div class="col-md-6">
+                <input id="contrasenya" type="password" class="form-control @error('password') is-invalid @enderror" name="contrasenya" required autocomplete="new-password">
+
+                @error('password')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
+            </div>
+        </div>
+
+        <div class="form-group row">
+            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirmar Contrasenya') }}</label>
+
+            <div class="col-md-6">
+                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+            </div>
+        </div>
+
+        <div class="form-group row mb-0">
+            <div class="col-md-6 offset-md-4">
+                <button id="submit" type="submit" class="btn btn-block btn-danger">
+                    {{ __('Guardar') }}
+                </button>
+            </div>
+        </div>
+    </form>
 @endsection
--- ProyectoLaravel/PFCTennis/resources/views/layouts/configuracio.blade.php
+++ ProyectoLaravel/PFCTennis/resources/views/layouts/configuracio.blade.php
@@ -1,19 +1,12 @@
 <!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html>
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>@yield('titol')</title>
-
-    <!-- Scripts -->
-    <!-- <script src="{{ asset('js/app.js') }}" defer></script> -->
-
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    
+    @stack('css')
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'>
@@ -22,8 +15,6 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script> 
 
-    <!-- Styles -->
-    <!-- <link href="{{ asset('css/app.css') }}" rel="stylesheet"> -->
 </head>
 <body>
     @if (!Auth::check())
@@ -32,97 +23,36 @@
         </script>
     @endif
 
-    <nav id="sidebarMenu" class="collapse navbar-expand-lg sidebar collapse bg-white">
-    <div class="position-sticky">
-      <div class="list-group list-group-flush mx-3 mt-4">
-        <!-- Collapse 1 -->
+  <nav id="sidebarMenu" class="collapse d-lg-block sidebar collapse collapse">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+          <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="position-sticky" id="collapsibleNavbar">
+      <div class="list-group list-group-flush mx-5 mt-4">
         <a
+          href="/index"
           class="list-group-item list-group-item-action py-2 ripple"
           aria-current="true"
-          data-mdb-toggle="collapse"
-          href="#collapseExample1"
-          aria-expanded="true"
-          aria-controls="collapseExample1"
         >
-          <i class="fas fa-tachometer-alt fa-fw me-3"></i>
+          <i class="fas fa-tachometer-alt fa-fw me-3"></i><span>Tornar</span>
         </a>
-        <!-- Collapsed content -->
-        <ul id="collapseExample1" class="collapse show list-group list-group-flush">
-          <li class="list-group-item py-1">
-            <a href="" class="text-reset">Link</a>
-          </li>
-          <li class="list-group-item py-1">
-            <a href="" class="text-reset">Link</a>
-          </li>
-          <li class="list-group-item py-1">
-            <a href="" class="text-reset">Link</a>
-          </li>
-          <li class="list-group-item py-1">
-            <a href="" class="text-reset">Link</a>
-          </li>
-        </ul>
+        <a href="#" class="list-group-item list-group-item-action py-2 ripple {{ Request::is('matricules') ? 'active' : '' }}">
+          <i class="fas fa-fw me-1"></i><span>Llistat matricules</span></a>
 
-        <!-- Collapsed content -->
-        <ul id="collapseExample2" class="collapse list-group list-group-flush">
-          <li class="list-group-item py-1">
-            <a href="" class="text-reset">Link</a>
-          </li>
-          <li class="list-group-item py-1">
-            <a href="" class="text-reset">Link</a>
-          </li>
-          <li class="list-group-item py-1">
-            <a href="" class="text-reset">Link</a>
-          </li>
-          <li class="list-group-item py-1">
-            <a href="" class="text-reset">Link</a>
-          </li>
-        </ul>
-        <!-- Collapse 2 -->
+        <a href="#" class="list-group-item list-group-item-action py-2 ripple {{ Request::is('home') ? 'active' : '' }}"
+          ><i class="fas fa-chart fa-fw me-3"></i><span>Configuració</span></a>
+
+        <a href="#" class="list-group-item list-group-item-action py-2 ripple"
+          ><i class="fas fa-chart-line fa-fw me-3"></i><span>Tancar Sessió</span></a>
       </div>
     </div>
   </nav>
-            <!-- <nav class="navbar navbar-expand-lg navbar-dark fixed-left">
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+  
+  </div class="container">
+    @yield('content')
+  </div>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav nav-fill w-100">  
-                        <li class="nav-item">
-                            <h4>Menú de {{ Auth::user()->nom }}</h4>
-                        </li>
-
-                        <li class="nav-item">
-                            <a href="{{ url('/index') }}">Tornar</a>
-                        </li>
-
-                        <li class="nav-item">
-                            <a href="#">Configuració</a><br />
-                        </li>
-
-                        <li class="nav-item">
-                            <a href="#">Llistat Matricules</a><br />
-                        </li>
-
-                        <li class="nav-item">
-                            <a class="" href="{{ route('logout') }}"
-                                onclick="event.preventDefault();
-                                                document.getElementById('logout-form').submit();">
-                                {{ __('Tancar Sessió') }}
-                            </a>
-
-                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                @csrf
-                            </form>
-                        </li>
-                    </ul>  
-                </div>
-            </nav> -->
-
-    </div>
-        <main class="py-4">
-            @yield('content')
-        </main>
-    </div>
+@stack('scripts')
 </body>
 </html>
